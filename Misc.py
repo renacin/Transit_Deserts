dc4c8a6c8b67b63cd49fcb89b3a88135a39f09e6
Renacin Matadeen <renacin.matadeen@ryerson.ca>
2019-02-20 16:24:04 -0500

    Should Use GeoPandas To Append Data, ARCGIS Takes Too Long

--- Misc.py
+++ Misc.py
@@ -10,5 +10,11 @@ import geopandas as gpd
 shp_path = r"/Users/renacinmatadeen/Downloads/Schools/Schools.shp"
 shp_data = gpd.read_file(shp_path)
 
-# Print Data
-print(shp_data.info())
+# Print Columns & General Info
+print(shp_data.info(), "\n")
+
+# Print CRS
+print(shp_data.crs, "\n")
+
+# Print Specific Column
+print(shp_data["TYPE"])
